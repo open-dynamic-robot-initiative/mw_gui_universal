31794b2403ac3bd1c2758a6e78a7ff98579b334d
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2016-08-02 11:35:58 +0200

    Set status label based on Flag_enableAlignment.

--- app.js
+++ app.js
@@ -24,58 +24,6 @@ function mrevToDegree(valueFromTarget) {
 	return (valueFromTarget * 360).toFixed(2);
 }
 
-//! Convert EST state value to its string representation.
-function convertEstStateToString(state)
-{
-	/*
-	 * from MW/sw/modules/est/src/est_states.h
-	 * 
-	  typedef enum
-		{
-		  EST_State_Error=0,            //!< error
-		  EST_State_Idle,               //!< idle
-		  EST_State_RoverL,             //!< R/L estimation
-		  EST_State_Rs,                 //!< Rs estimation state
-		  EST_State_RampUp,             //!< ramp up the speed
-		#if !defined(FAST_ROM_V1p6) && !defined(FAST_ROM_V1p7)
-		  EST_State_ConstSpeed,         //!< constant speed after ramp up
-		#endif
-		  EST_State_IdRated,            //!< control Id and estimate the rated flux
-		  EST_State_RatedFlux_OL,       //!< estimate the open loop rated flux
-		  EST_State_RatedFlux,          //!< estimate the rated flux 
-		  EST_State_RampDown,           //!< ramp down the speed 
-		  EST_State_LockRotor,          //!< lock the rotor
-		  EST_State_Ls,                 //!< stator inductance estimation state
-		  EST_State_Rr,                 //!< rotor resistance estimation state
-		  EST_State_MotorIdentified,    //!< motor identified state
-		  EST_State_OnLine,             //!< online parameter estimation
-		  EST_numStates                 //!< the number of estimator states
-		} EST_State_e;
-		
-		NOTE: Assume the #if is false
-	 */
-	
-	const state_names = [
-		  "EST_State_Error",
-		  "EST_State_Idle",
-		  "EST_State_RoverL",
-		  "EST_State_Rs",
-		  "EST_State_RampUp",
-		  "EST_State_IdRated",
-		  "EST_State_RatedFlux_OL",
-		  "EST_State_RatedFlux",
-		  "EST_State_RampDown",
-		  "EST_State_LockRotor",
-		  "EST_State_Ls",
-		  "EST_State_Rr",
-		  "EST_State_MotorIdentified",
-		  "EST_State_OnLine",
-		  "EST_numStates"
-		  ];
-	
-	return state_names[state];
-}
-
 //! make sure spring stiffnes value is a positive number.
 function validateVirtualSpringStiffness(value)
 {
@@ -83,4 +31,14 @@ function validateVirtualSpringStiffness(value)
 		return 0;
 	else
 		return value;
+}
+
+function onAlignMotor1PropertyChanged( propertyName, newValue, oldValue) {
+	dijit.byId("lbl_alignMotor1").set("label",
+			($TI.GUIVars.getValue("alignMotor1") ? "Aligning..." : "Ready!"));
+}
+
+function onAlignMotor2PropertyChanged( propertyName, newValue, oldValue) {
+	dijit.byId("lbl_alignMotor2").set("label",
+			($TI.GUIVars.getValue("alignMotor2") ? "Aligning..." : "Ready!"));
 }
